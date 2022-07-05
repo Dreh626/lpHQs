de4b556c90f5659d58be11bf1e1660d0b2b4b159
Dreh626 <garcia.andref@gmail.com>
2022-07-04 23:07:46 -0300

    v7-menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,80 +1,124 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles, AppBar, IconButton, Toolbar, Collapse } from '@material-ui/core';
-import {Sort, ExpandMore} from '@material-ui/icons';
+import { Sort, ExpandMore } from '@material-ui/icons';
 import { Link as Scroll } from "react-scroll";
-
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 
 const useStyles = makeStyles((theme) => ({
 
-    root:{
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        height:'100vh',
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
         fontFamily: 'Nunito',
 
     },
-    appbar:{
+    appbar: {
         background: 'rgba(0, 0, 0, 0.2)',
         boxShadow: 'none !important',
-        
+
     },
-    toolbar:{
+    toolbar: {
         width: '80%',
         margin: '0 auto',
     },
-    hq:{
+    hq: {
         color: '#FF7020',
         fontSize: '1.2em',
         fonWeight: '700',
     },
-    appTitle:{
+    appTitle: {
         flex: '1',
     },
-    icon:{
+    icon: {
         color: '#FFF',
         fontSize: '1em',
     },
-    container:{
+    container: {
         color: '#FFF',
         fontWeight: 'bold',
         textAlign: 'center',
-        fontSize:'3rem',
+        fontSize: '3rem',
     },
-    goDown:{
+    goDown: {
         color: '#FF7020',
         fontSize: '4rem',
     }
 }));
 
-export default function Header(){
+const options = [
+    'Menu',
+    'About',
+    'Contact'
+
+];
+
+const ITEM_HEIGHT = 48;
+
+export default function Header() {
     const classes = useStyles();
     const [checked, setChecked] = useState<boolean>(false);
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+
+
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     useEffect(() => {
         setChecked(true);
-    },[])
+    }, [])
 
     return (
         <div className={classes.root} id="header">
             <AppBar className={classes.appbar}>
                 <Toolbar className={classes.toolbar}>
-                <h1 className={classes.appTitle}>Fórum <span className={classes.hq}>HQ</span></h1>
-                <IconButton>
-                    <Sort className={classes.icon}/>
-                </IconButton>
+                    <h1 className={classes.appTitle}>Fórum <span className={classes.hq}>HQ</span></h1>
+                    <IconButton aria-label="more"
+                        aria-controls="long-menu"
+                        aria-haspopup="true"
+                        onClick={handleClick}>
+                        <Sort className={classes.icon} />
+                    </IconButton>
+                    <Menu
+                        id="long-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={open}
+                        onClose={handleClose}
+                        PaperProps={{
+                            style: {
+                                maxHeight: ITEM_HEIGHT * 4.5,
+                                width: '20ch',
+                            },
+                        }}
+                    >
+                        {options.map((option) => (
+                            <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+                                {option}
+                            </MenuItem>
+                        ))}
+                    </Menu>
                 </Toolbar>
             </AppBar>
-            <Collapse in={checked} collapsedSize={50} {...(checked ? {timeout: 1000} : {})}>
-            <div className={classes.container}>
-                <h2 >Noticias sobre<br /> <span className={classes.hq}>HQs</span></h2>
-                <Scroll to="notice-to-visit" smooth={true}>
-                <IconButton>
-                    <ExpandMore className={classes.goDown}/>
-                </IconButton>
-                </Scroll>
-            </div>
+            <Collapse in={checked} collapsedSize={50} {...(checked ? { timeout: 1000 } : {})}>
+                <div className={classes.container}>
+                    <h2 >Noticias sobre<br /> <span className={classes.hq}>HQs</span></h2>
+                    <Scroll to="notice-to-visit" smooth={true}>
+                        <IconButton>
+                            <ExpandMore className={classes.goDown} />
+                        </IconButton>
+                    </Scroll>
+                </div>
             </Collapse>
         </div>
     )
